96f8d9497127313e7b0efda3a0f76b34514852db
deluxghost <deluxghost@users.noreply.github.com>
2018-08-23 18:26:47 +0800

    Create startup.zsh

---
+++ .zsh_custom/startup.zsh
@@ -0,0 +1,46 @@
+ZSH_THEME="deluxghost"
+COMPLETION_WAITING_DOTS="true"
+HIST_STAMPS="yyyy-mm-dd"
+HYPHEN_INSENSITIVE="true"
+export UPDATE_ZSH_DAYS=5
+setopt noincappendhistory
+setopt nosharehistory
+
+source ~/bin/antigen.zsh
+antigen use oh-my-zsh
+antigen bundle git
+antigen bundle pip
+antigen bundle tmux
+antigen bundle mosh
+antigen bundle python
+antigen bundle docker
+antigen bundle systemd
+antigen bundle common-aliases
+antigen bundle colored-man-pages
+antigen bundle command-not-found
+antigen bundle Tarrasch/zsh-bd
+antigen bundle zdharma/fast-syntax-highlighting
+antigen bundle zdharma/history-search-multi-word
+antigen apply
+
+function is_wsl()
+{
+    grep -q Microsoft /proc/version &> /dev/null
+    return $?
+}
+
+function mcdir()
+{
+    mkdir -p -- "$1" && cd -P -- "$1"
+}
+
+export EDITOR=vim
+export PATH=$PATH:~/bin
+
+alias esrc='vi $ZSH_CUSTOM/startup.zsh'
+alias resrc='source ~/.zshrc'
+alias ls='ls --color'
+alias la='ls -a'
+alias ll='ls -l'
+alias lla='ls -la'
+alias rr='rm -r'
